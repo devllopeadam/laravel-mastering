a928b5cf2aa58df58736acc3483c9e43ffcc41a1
devllopeadam <jeniahadam@gmail.com>
2025-02-11 22:27:01 +0100

    share tp with class

--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-// Route::get("/{name}", UserController::class . "@getName");
-
-// Route::get("sayHello/{name}", UserController::class . "@sayHello")->middleware(AuthMiddleware::class);
-
-// Route::get('sayHello', UserController::class . "@getName");
-
-
-
-
-Route::get("/profile/{name?}", [ProfileController::class, "showProfile"]);
-
-Route::get("/flights", [FlightController::class, "showFlights"]);
-
-Route::get("/users", [UserController::class, "showUsers"])->middleware(UserMiddleware::class);
-
-
-Route::get("/posts", [PostController::class, "showPosts"]);
-
-Route::get("/posts/{id}", [PostController::class, "showPostById"]);
-
-Route::get("/films", [FilmController::class, "getFilms"]);
-
-Route::get("/filmsName", [FilmController::class, "getFilmsName"]);
-
-Route::get("/filmsAfterDate", [FilmController::class, "getFilmsTitreDate"]);
